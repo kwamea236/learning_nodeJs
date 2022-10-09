2f66e0eb3b8c2b4a05c56fa4a9f2117275d3a125
kwamea236 <kwamea236@gmail.com>
2022-10-09 11:46:21 +0000

    handlebars added to app.js

--- app.js
+++ app.js
@@ -1,24 +1,69 @@
+/**
+ * Author: Kwame Ato
+ * Date: 9th October, 2022
+ */
+
+const expressHandlebars=require("express-handlebars");
+
 const express = require("express");
-const res = require("express/lib/response");
+
 const httpStatus = require("http-status-codes");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get((req, res)=>{
-    res.type("text/plain");
-    res.status(httpStatus.StatusCodes.NOT_FOUND);
-    res.send("404 notfound")
+
+/**
+ * express handlebars configuration
+ * 
+ * *********************************
+ * 
+ * handlebars is a page template engine
+ * used to render static and dynamic html pages
+ */
+
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: "main",
+}));
+
+app.set("view engine", "handlebars");
+
+//end of handlebar engine configuration
+
+/**
+ * 
+ * 
+ */
+
+app.get("/", (req, res)=>{
+    res.render("home");
 })
+
+app.get("/about",(req, res)=>{
+    res.render("about");
+})
+
+
+/**
+ * custome eror handling
+ */
+
+app.use((req, res)=>{
+    res.status(httpStatus.StatusCodes.NOT_FOUND);
+    res.render("404");
+});
+
+//custome 500 internal server error
 
 app.use((err,req, res,next)=>{
-    res.type("text/plain");
+    console.error(err.message);
     res.status(httpStatus.StatusCodes.INTERNAL_SERVER_ERROR);
-    res.send("404 notfound")
+    res.render("500");
 })
 
+//listening port
 app.listen(port, ()=>{
-    console.log(`Express start on http://localhost:${port}`+
-    ` press ctrl-c to terminate`);
+    console.log(`Express start on http://localhost:${port}\n`+
+    `press ctrl-c to terminate`);
 });
